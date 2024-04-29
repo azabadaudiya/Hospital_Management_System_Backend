95f44fb6f6d9ec8e7bbf13152bc4d4a3c385095c
azabadaudiya <146463403+azabadaudiya@users.noreply.github.com>
2024-04-29 20:16:59 +0530

    Update dbConnection.js

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = () =>{
   mongoose.connect(process.env.MONGO_URI, {
-    dbName:"HOSPITAL_MANAGEMENT",
+    dbName:"MERN_HOSPITAL_MANAGEMENT",
   }).then(()=>{
     console.log("connected to database");
   }).catch(()=>{
